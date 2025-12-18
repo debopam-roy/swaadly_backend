186ff2ae2c856b4dc8069c0d724ad19a9f0b454c
debopam-roy <debo.roy79@gmail.com>
2025-12-18 22:29:13 +0530

    completed

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: parseInt(process.env.APP_PORT || '5000', 10),
+  port: parseInt(process.env.APP_PORT || '8080', 10),
   frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000',
   dbUrl: process.env.DATABASE_URL || '',
   jwtSecret: process.env.JWT_SECRET || 'default_jwt_secret',
